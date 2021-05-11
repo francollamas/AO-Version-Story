31288da59a450ee2867c86a2b0d9831bb45358f1
Franco Llamas <francollamas077@gmail.com>
2021-05-11 03:44:48 -0300

    Agregado script para curar archivos
    El script convierte todo el codigo a minusculas para asi no tener problemas de
    comparacion ya que al trabajar en VB6, es comun que se cambien los cases constantemente.
    Ademas, quita los archivos .frx que son innecesarios para el fin del repositorio.

---
+++ CurarArchivos.java
@@ -0,0 +1,47 @@
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CurarArchivos {
+    public static void main(String[] args) {
+        
+        var clientFiles = new File("./cliente").listFiles();
+        var serverFiles = new File("./servidor").listFiles();
+        
+        var files = new File[clientFiles.length + serverFiles.length];
+        System.arraycopy(clientFiles, 0, files, 0, clientFiles.length);
+        System.arraycopy(serverFiles, 0, files, clientFiles.length, serverFiles.length);
+
+        for (var file : files) {
+            if (file.getPath().toLowerCase().endsWith(".frx")) {
+                file.delete();
+                continue;
+            }
+            try {
+                file.renameTo(new File(file.getPath().toLowerCase()));
+        
+                var fis = new FileInputStream(file);
+                var bytes = fis.readAllBytes();
+                fis.close();
+
+
+                var toLowerCase = new String(bytes).toLowerCase();
+
+                var fos = new FileOutputStream(file);
+                fos.write(toLowerCase.getBytes());
+                fos.close();
+
+            } catch (FileNotFoundException e) {
+                
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Listo!");
+    }
+}
